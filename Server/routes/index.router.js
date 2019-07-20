1809ead3ed610d1dd9ce880c6c3122890aba5e43
prakashgitrepo <50910927+prakashgitrepo@users.noreply.github.com>
2019-07-20 15:20:27 +0530

    Update index.router.js

--- Server/routes/index.router.js
+++ Server/routes/index.router.js
@@ -1,8 +1,7 @@
-// routed only for HR Managing data
+
 const express = require('express');
 const router = express.Router();
-const hrdata = require('../models/hr.model');
-const candidate = require('../models/candiatate.model')
+
 const ctrlUser = require('../controllers/user.controller');
 
 router.post('/register', ctrlUser.register);
@@ -11,34 +10,6 @@ router.post('/userProfile', ctrlUser.userProfile);
 router.post('/hradd', ctrlUser.hradd);
 
 
-router.post('/hradd', (req, res) => {
-        let { jobName, description } = req.body;
-        let errors = [];
-       // Validate Fields
-        if(!jobName) {
-          errors.push({ text: 'Please add the jobname' });
-        }
-        
-        if(!description) {
-          errors.push({ text: 'Please add a description' });
-        }
-         // Check for errors
-        if(errors.length > 0) {
-          res.render('add', {
-            errors,
-            jobName,  
-            description, 
-          });
-        } else {
-          // Insert into table
-          hrdata.create({
-            jobName,
-            description 
-          })
-            .then(hrdatas => res.redirect('/'))
-            .catch(err => console.log(err));
-        }
-      });
 
     
 module.exports = router;
